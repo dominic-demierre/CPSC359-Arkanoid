ea77ea1e0a450c40ce2019b9a0ed4011cae905ca
Glenn <skeltong2@gmail.com>
2018-06-01 22:17:06 -0600

    added build, source, makefile and main.s in source has been started

---
+++ source/main.s
@@ -0,0 +1,32 @@
+/*****************************************************
+ * CPSC 359 Assignment 2, Arkanoid
+ * Dominic (last name), Maha (last name), 
+ * Jessica (last name), Glenn Skelton
+ *
+ * Due June 12, 2018
+ *
+ *****************************************************/
+
+.sect	.data
+
+
+
+
+
+/*----------------------- CODE ----------------------*/
+.sect	.data
+.global	main
+
+main:
+
+
+
+
+
+
+
+
+end:
+	bl	exit
+
+.end
