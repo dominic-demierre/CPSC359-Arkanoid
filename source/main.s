71a50841e5fdec3b7c66c80802363a32f34f6443
Glenn <skeltong2@gmail.com>
2018-06-04 17:11:11 +0000

    made the print function global and the image file, added a test to make sure it works. It does

--- source/main.s
+++ source/main.s
@@ -9,6 +9,12 @@
 
 .sect	.data
 
+.align
+.globl	frameBufferInfo
+frameBufferInfo:
+	.int	0		@ frame buffer pointer
+	.int	0		@ screen width
+	.int	0		@ screen hight
 
 
 
@@ -19,14 +25,42 @@
 
 main:
 
+	ldr	r0, =frameBufferInfo
+	bl	initFbInfo
+	
 
-
-
-
-
+	@ call print background to test functionality of linking
+	bl	printBackground
 
 
 end:
 	bl	exit
 
+
+
+
+@ eventually move to seperate file
+/*---------------------- FUNCTIONS --------------------*/
+
+/*****************************************************
+ * Purpose: To print the main background image.
+ *
+ *
+ *
+ *
+ *****************************************************/
+printBackground:
+	push	{lr}
+
+	bl	getCoord		@ get coordinate of the center + offset for pixels
+	bl	printImage		@ return value is the params for next
+		
+	pop 	{lr}
+	bx	lr
+
+
+
+
 .end
+
+
