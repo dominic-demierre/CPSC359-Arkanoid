1fd74ad7e44abec165f4fe3fd7108e393b9e4ebf
Glenn <skeltong2@gmail.com>
2018-06-08 19:53:59 +0000

    completed the main screen

--- source/main.s
+++ source/main.s
@@ -41,15 +41,23 @@ resetPositions:
 .global	main
 
 main:
-	bl	setup
-	
-	@ run tests
-	bl	testBackgrounds
-	
-	bl	testPaddle
-	
+	bl	setup				@ set up all buffers for the game proccesses
+	@ print the starting screen		
+
+	@ get user input to determine mode
+	bl 	mainMenu			@ get user input for starting or quiting the game
+	@ will only go on if select is pressed else it will terminate, r0 will have 0 or 1 
+	cmp	r0, #0				@ if the function mainMenu returned 0, terminate the program
+	beq	end				@ exit the program, will need to write a blank screen
+	@ if function returned 1, go on to the game loop
+
+gameLoop:
+
+
+
+
 end:
-	bl	exit
+	bl	exit				@ terminate the program
 
 
 
@@ -84,6 +92,97 @@ setup:
 	bl	init_GPIO			@ set pin 10 to input
 
 	pop	{lr}
+	bx	lr
+
+/******************************************************
+ * Main menu loop
+ * call mainMenu function to wait for user input to start or exit
+ *
+ * check to see if up or down is pressed or A to go onto game loop
+ * if nothing keep polling for input
+ * return value will be 1 for going to main menu or 0 to quit
+ ******************************************************/
+mainMenu:	
+	push	{r4, r5, lr}
+	
+	mov	r5, #0				@ defualt 0 for start screen and 1 for quit selection
+
+	@ start with the start button selected
+	ldr	r2, =splashStart
+	bl	printBacking
+
+readInLoop:
+	bl	Read_SNES			@ get the input from the SNES paddle
+	mov	r1, #0xFFFF			@ mask to check if a button was pushed or not
+	teq	r0, r1				@ test to see if a button was pushed
+	beq	readInLoop			@ if not go back and wait until it one is pushed
+	
+btnPressed:
+	@ r0 contains the buttons pressed
+	mov	r4, r0				@ store the button values in r4	for any function calls
+
+	mvn	r0, r0				@ get the compliment of r0
+	mov	r1, #0x10			@ for seeing if up was pressed
+	mov	r2, #0x20			@ for seeing if down was pressed
+	mov	r3, #0x100			@ value to see if A was pressed
+	and	r1, r1, r0			@ extract the bit for up
+	and	r2, r2, r0			@ extract the bit for down
+	and	r3, r3, r0			@ extract the bit for down
+	lsr	r1, #4				@ shift the bit for testing if on for up
+	lsr	r2, #5				@ shift the bit for testing if on for down
+	lsr	r3, #8				@ shift the bit for testing if on for A
+	
+	@ test if A was pressed first to see if it should go on
+	teq	r3, #1				@ see if bit is turned on
+	beq	aPressed			@ got to A pressed label if true to determine what mode to execute 
+	teq	r2, #1				@ see if bit is turned on for DOWN
+	beq	checkScreenFlag			@ if so determine which screen to change
+ 	teq	r1, #1				@ see if bit is turned on for UP
+	beq	checkScreenFlag			@ if so determine which screen to change
+	b	readOutLoop			@ if nothing valid was pressed, continue to read
+
+checkScreenFlag:
+	cmp	r5, #0				@ test to see what the flag bit is set to 
+	beq	quitScreen			@ if flag is set to 0, got to quitScreen to change to quit screen image
+	bne	startScreen			@ if flag is set to 1, got to startScreen to change to start screen image
+	b	readOutLoop			@ error so exit
+
+startScreen:
+	teq	r2, #1				@ make sure that up wasn't pressed
+	beq	readOutLoop			@ if so, continue back to reading input
+	ldr	r2, =splashStart		@ get the start screen address
+	bl	printBacking			@ print the start screen
+	mov	r5, #0				@ change flag to 0 to signify that start screen is now enabled
+	b	readOutLoop
+
+quitScreen:
+	teq	r1, #1				@ make sure that down wasn't pressed
+	beq	readOutLoop			@ if so, continue back to reading input
+	ldr	r2, =splashQuit			@ get the exit screen address
+	bl	printBacking			@ print the exit screen
+	mov	r5, #1				@ change flag to 1 to signify that quit screen is now enabled
+	b	readOutLoop
+
+aPressed:
+	cmp	r5, #0				@ see which screen we are currently on
+	moveq	r0, #1				@ if start was selected, return value of 1 to start game
+	movne	r0, #0				@ if quit was selected, return 0 to terminate the game
+	b	endMainLoop			@ terminate main menu loop	
+
+readOutLoop:
+	@ delay to eliminate any accidental multiple prints from odd elecetrical connection
+	mov	r0, #5000			@ value to delay, multiplied by 2
+	lsl 	r0, #1				@ delay is 10 miliseconds
+	bl	delayMicroseconds		@ delay for 10 miliseconds
+
+	bl	Read_SNES			@ get the input from the SNES paddle
+	mov	r1, #0xFFFF			@ mask to check if a button was released
+	cmp	r0, r1				@ test to see if a button was pushed	
+	bne	readOutLoop			@ if not go back and wait until it one is pushed
+	b	readInLoop			@ loop back to get input
+	
+endMainLoop:
+	pop	{r4, r5, lr}
 	bx	lr
 
 /******************************************************
